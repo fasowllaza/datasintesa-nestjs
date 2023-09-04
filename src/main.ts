3a459bf2584a4c6c921e714438f3dbdde01e0112
Fakhreza Dwiputra Suparyadi <52316053+fasowllaza@users.noreply.github.com>
2023-09-04 08:58:07 +0700

    Fixing Response into availability and result time only

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,6 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   dotenv.config(); 
   const app = await NestFactory.create(AppModule, {cors: true});
-  await app.listen(process.env.PORT || 3000);
+  await app.listen(process.env.PORT || 3001);
 }
 bootstrap();
