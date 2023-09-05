be7b193526e091ecdf5a6e9853f20dea424e62b0
Fakhreza Dwiputra Suparyadi <52316053+fasowllaza@users.noreply.github.com>
2023-09-05 11:26:58 +0700

    [Bugfix] Fixing Queries

--- src/raw-data/raw-data.repository.ts
+++ src/raw-data/raw-data.repository.ts
@@ -17,10 +17,11 @@ export class RawDataRepository {
     let queries:RawDataBody = {}
     for (const query in data) {
       if(data[query]) {
-        if(query === 'startDate') {
-          queries[query] = { $gte:data[query] }
-        } else if (query === 'endDate') {
-          queries[query] = { $lte:data[query] }
+        if(query === 'startDate' || query ==='endDate') {
+          queries['resultTime'] = { 
+            $gte: new Date (data['startDate']),
+            $lte: data['endDate'] ? new Date (data['endDate']) : new Date()
+          }
         } else {
           queries[query] = data[query]
         }
